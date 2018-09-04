adfc71b85f9a3bf0af80ffb9f8e9ce9de03f97ba
Yash <yashrsharma44@gmail.com>
2018-09-04 14:56:29 +0530

    Added the functions for scraping the data

--- scraper.py
+++ scraper.py
@@ -1,11 +1,22 @@
-from main import getPage, getHeader, getLinks, getSummary, getScoreCard, getTestLinks
-from utils import matchHeader
+from utils import getPage, getHeader, getLinks, getSummary, getScoreCard, getTestLinks, finalScore,matchHeader 
 import click
 from bs4 import BeautifulSoup as BS
 
 
 @click.command()
 def display_message():
+	'''
+	This method displays the list of live matches going on
+	'''
+	'''
+	Arguments
+	---------
+	None
+
+	Returns
+	-------
+	None : List of live matches
+	'''
 
 	soup = getPage()
 	headers = getHeader(soup)
@@ -18,8 +29,21 @@ def display_message():
 		else:
 			click.secho('{} | {} '.format(index, match), fg='yellow')
 @click.command()
-@click.option('--ch', default=1, help='Enter the channel number to display the summary')
+@click.option('--ch', default=1, help='Enter the channel number with --ch to display the summary')
 def printSummary(ch):
+	'''
+	This method takes a channel number,
+	and returns the summary of the match
+	'''
+	'''
+	Arguments
+	---------
+	int : channel number
+
+	Returns
+	-------
+	None : Output
+	'''
 
 	soup = getPage()
 	links = getLinks(soup)
@@ -41,35 +65,69 @@ def printSummary(ch):
 			yield i
 
 	gendata = iterator(data)
-	click.secho('{}'.format(matches[ch-1]), fg='yellow')
+	click.secho('{}\n'.format(matches[ch-1]), fg='yellow')
 	for index,i in enumerate(gendata):
 		if index == int(len(data)/4):
-			click.secho('------------------------------------', fg='red')
-			click.secho('{}  |   {}'.format(i.string, next(gendata).string), fg='red')
+			click.secho(' __________________________________________', fg='red')
+			click.secho(' {}     {}'.format(i.string, next(gendata).string), fg='red')
 		else:
-			click.secho('{}  |   {}'.format(i.string, next(gendata).string), fg='red')
+			run = next(gendata).string
+			click.secho(' {}'.format(i.string)+' '*(20-len(i.string))+'|{}'.format(run), fg='red')
 
 @click.command()
 @click.option('--ch', default=1, help='Enter the channel number with --ch for scorecard')
 def printScoreCard(ch):
+	'''
+	This method takes in a channel number,
+	and returns the Scorecard for a given
+	match number
+	'''
+	'''
+	Arguments
+	---------
+	int : Channel number
+
+	Returns
+	-------
+	None : Displays the output
+	'''
 
 	soup = getPage()
 	links = getLinks(soup)
 
+	headers = getHeader(soup)
+	matches = matchHeader(headers)
+
 	link = links[ch-1].find_all('a')[2]['href']
 	scorelinks = getTestLinks(link)
 	
-	if scorelinks:
-		for link in scorelinks:
-			data = getScoreCard(link)
-
-			for player in data:
-				details = player.strings
-				try:
-					click.secho('{}  |  {}({}) | {} | {} | {}'.format(next(details),next(details), next(details),next(details),next(details),next(details)), fg='red')
-				except StopIteration:
-					break
-			click.secho('___________________________________' ,fg='red')
+	click.secho('{}'.format(matches[ch-1]), fg='yellow')
+	
+	for link in scorelinks:
+		data = getScoreCard(link)
+		displayline=None
+		click.secho(' Batsman            |R(B)      | 4 | 6   | S/R',fg='blue')
+		for player in data:
+			details = player.strings
+			
+			try:
+				name = next(details)
+				run = next(details)
+				ball = next(details)
+				four = next(details)
+				six = next(details)
+				sr = next(details)
+				text = '{}'.format(name)+' '*(20-len(name))+'| {}({})'.format(run,ball)\
+				+' '*(7-len(run)-len(ball))+'| {} | {} '.format(four,six)+' '*(4-len(four)-len(six))+'| {}'.format(four,six,sr)
+				click.secho(text, fg='red')
+			except StopIteration:
+				break
+		click.secho('_'*(len(text)), fg='red')
+		score = finalScore(link)
+		
+		for details in score:
+			click.secho(' {} '.format(details), fg='red')
+		click.secho('_'*(len(text)) ,fg='red')
 
 if __name__ == '__main__':
-	printScoreCard()
+	display_message()
