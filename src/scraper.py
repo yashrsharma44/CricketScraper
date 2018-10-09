bf1d4b0e7d5402766024d42f165ff2c0d880ac25
Yash <yashrsharma44@gmail.com>
2018-10-09 18:56:00 +0530

    Added colors

--- src/scraper.py
+++ src/scraper.py
@@ -1,24 +1,28 @@
 import click
+import sys
 
 from display import display_message
 from summary import printSummary
 from scorecard import printScoreCard
 
 if __name__ == '__main__':
-	click.secho('Please enter the choices:')
-	click.secho('Enter 1 to get the list of scores')
-	click.secho('Enter 2 to get the Summary for a given channel')
-	click.secho('Enter 3 to get the ScoreCard for a given channel')
+	click.secho('Please enter the choices:',fg='red')
+	click.secho('Enter 1 to get the list of scores',fg='green')
+	click.secho('Enter 2 to get the Summary for a given channel',fg='green')
+	click.secho('Enter 3 to get the ScoreCard for a given channel',fg='green')
 	try:
 		opt = click.prompt('Enter the option',type=int)
+	
+		if opt == 1:
+			display_message()
+		elif opt == 2:
+			ch = click.prompt('Enter the Channel number',type=int)
+			printSummary(ch)
+		elif opt==3:
+			ch = click.prompt('Enter the Channel number',type=int)
+			printScoreCard(ch)
+		else:
+			raise Exception
 	except Exception:
-		click.secho('Invalid Input!! Please try again')
+		click.secho('Invalid Input!! Please try again',fg='red')
 		sys.exit()
-	if opt == 1:
-		display_message()
-	elif opt == 2:
-		ch = click.prompt('Enter the Channel number',type=int)
-		printSummary(ch)
-	else:
-		ch = click.prompt('Enter the Channel number',type=int)
-		printScoreCard(ch)
